4694bce1f6eda0d3fb561940801cba81b086a995
anilbillupati <billupatianil@gmail.com>
2025-04-14 23:09:39 +0530

    python practice

---
+++ multipleAssignments.py
@@ -0,0 +1,24 @@
+msg1 , msg2 = "hello" ,"world"  #msg1=hello ,msg2=world 
+print(msg1,msg2)
+
+msg1,msg2 =msg2,msg1 #same like msg1= msg2  and msg2=msg1
+print(msg1,msg2)
+
+print("virat kohli")
+
+# second scenario to practice swapping 
+str1,str2="world","hello"
+str3,str4,str5="name","is","my"
+
+str1,str2=str2,str1 
+
+str3,str4,str5 =str5,str3,str4 
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
+
+
+
---
+++ namingConventions.py
@@ -0,0 +1,7 @@
+capital_cit_of_India="Delhi"
+capital_City_of_America="WashingtonDC"
+capital_cit_of_Australia="Sydney"
+
+print(capital_cit_of_India)
+print(capital_City_of_America)
+print(capital_cit_of_Australia)
