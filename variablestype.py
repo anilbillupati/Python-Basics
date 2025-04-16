3f1ff3772e3a585a51bcc6ec89a4279610ff1b11
anilbillupati <billupatianil@gmail.com>
2025-04-16 23:41:21 +0530

    changes are added

--- variablestype.py
+++ variablestype.py
@@ -16,3 +16,6 @@ print ("Anil Kumar",str)
 # Type Casting a variable in python can be converted into other datatypes called type casting 
 a=10.234    
 print("converted to int " ,int(a))
+
+# type error occurs when we tried to change the the incompatable datatypes like string to int ,
+# but changes when the string it self consists of only numbers
