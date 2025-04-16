6afe77bba56f35fa498ebe5bf2880b022b503f9a
anilbillupati <billupatianil@gmail.com>
2025-04-16 23:11:37 +0530

    datatypes and convertion

---
+++ variablestype.py
@@ -0,0 +1,18 @@
+print(type("anil"))
+print(type(12))
+print(type([1,2,3,4,5,6,7]))
+print(type(13.444))
+print(type(True)) # Boolean values : True & False  both are having the uppercase letter at the starting 
+
+
+# dynamic typing supports in python 
+# variables type can be changed from one type to another type 
+
+str =10 
+print ("Anil Kumar",str)
+str= "Billupati"
+print ("Anil Kumar",str)
+
+# Type Casting a variable in python can be converted into other datatypes called type casting 
+a=10.234    
+print("converted to int " ,int(a))
